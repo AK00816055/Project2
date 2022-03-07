6011271b8d3f5419cf1f1fa02105b323cd036f9e
Ak00816055 <avnishkmr1609@gmail.com>
2022-03-07 09:40:21 +0000

    this is the first commit

---
+++ employee-project-two/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO employee(id,name,designation,salary,age) values(101,'Avnish','manager',120000,24);
+INSERT INTO employee(id,name,designation,salary,age) values(102,'Raadha','CEO',1200000,22);
+INSERT INTO employee(id,name,designation,salary,age) values(103,'Priya','Developer',1200000,28);
+INSERT INTO employee(id,name,designation,salary,age) values(104,'Ammy','CA',3600000,24);
+
+INSERT INTO employee(id,name,designation,salary,age) values(105,'RAvi','manager',400,24);
+INSERT INTO employee(id,name,designation,salary,age) values(106,'Shyam','CEO',410,99);
+INSERT INTO employee(id,name,designation,salary,age) values(107,'Ram','Developer',420,182);
+INSERT INTO employee(id,name,designation,salary,age) values(108,'Sita','CV',430,69);
